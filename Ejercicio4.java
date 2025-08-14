d4b3b33e29f0852f8b8bc1338913c4e0992ada34
juivanm <juanivanvelazquezcabello@gmail.com>
2025-08-14 13:49:00 -0600

    Agregando comentarios

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Ejercicio4 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int opcion = 0;
-        do {
+        Scanner sc = new Scanner(System.in); //Creando scanner para leer datos
+        int opcion = 0; // Guardar lo que ingrese el usuario, la declaro fuera del do while para evitar que se este creando cada vez la opción
+        do { //Uso ciclo do para que se ejecute por lo menos una vez y asi pedir la opción del usuario para comparar en switch
             System.out.println("-- Opciones 1: Actualizar, 2: Eliminar, 3: Crear, 4: Listar, 5: Salir --");
             System.out.println("Ingresa la opción");
             opcion = sc.nextInt();
@@ -27,8 +27,9 @@ public class Ejercicio4 {
                     System.out.println("Error eligue entre las opciones disponibles");
                     break;
             }
-        } while (opcion != 5);
-        sc.close();
+        } while (opcion != 5); //Mientras la opción mo sea 5 se sigue repitiendo
+        sc.close(); //Cierro scanner
         System.out.println("Haz salido con exito!");
     }
 }
+
